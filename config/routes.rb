d0e3606751d1b2a0823fc1816d31ac613220290f
Hiroaki <hiroaki.fukuoka@gmail.com>
2018-05-22 15:52:18 +0900

    implemented task crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "tasks", to: "tasks#index", as: :tasks
-  get "tasks/:id", to: "tasks#show", as: :task
-  get "tasks/new", to: "tasks#new", as: :new_task
-  post "tasks", to: "tasks#create", as: :create_task
+
+  resources :tasks
+
+  # get "tasks", to: "tasks#index", as: :tasks
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # post "tasks", to: "tasks#create", as: :create_task
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update", as: :update_task
+  # delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
 end
